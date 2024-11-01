0b737309d5ffd21a8c5c1aed01b62f384c49670a
JoaoVictorCS16 <joaovictor.dev16@gmail.com>
2024-10-31 22:13:41 -0300

    imc calculadora

---
+++ imc_calculadora.py
@@ -0,0 +1,21 @@
+#Atividade lógica de programação: Programa para calcular IMC (Índice de Massa Corpórea)
+
+peso = float(input("Digite seu peso em kg:"))
+altura = float(input("Digite sua altura em metros: "))
+
+imc = peso / (altura ** 2)
+
+print(f"Seu IMC é: {imc: .2f}")
+
+if imc < 18.5:
+  print("Classificação: Abaixo do peso")
+elif 18.5 <= imc < 24.9:
+  print("Classificação: Peso normal")
+elif 25 <= imc < 29.9:
+    print("Classificação: Sobrepeso")
+elif 30 <= imc < 34.9:
+    print("Classificação: Obesidade grau I")
+elif 35 <= imc < 39.9:
+    print("Classificação: Obesidade grau II")
+else:
+    print("Classificação: Obesidade grau III (Obesidade Mórbida)")
