6c2c57c02a11dae118b1ed9b417a505e308298e5
JoaoVictorCS16 <joaovictor.dev16@gmail.com>
2024-10-31 22:36:49 -0300

    commit google cloud

--- imc_calculadora.py
+++ imc_calculadora.py
@@ -4,7 +4,7 @@ peso = float(input("Digite seu peso em kg:"))
 altura = float(input("Digite sua altura em metros: "))
 
 imc = peso / (altura ** 2)
-
+  
 print(f"Seu IMC é: {imc: .2f}")
 
 if imc < 18.5:
